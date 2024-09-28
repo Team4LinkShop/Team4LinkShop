3ea1d9714bac55206b3692d6f9c45083ec950a42
April-37 <mm37april@gmail.com>
2024-09-29 02:20:08 +0900

    feat : 메인페이지 헤더 컨테이너 추가

---
+++ src/pages/ShopList.jsx
@@ -0,0 +1,13 @@
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
+
+function ShopList() {
+  return (
+    <div>
+      <div css={containerHeader}>
+        <Logo />
+        <BtnCreate />
+      </div>
+    </div>
+  );
+}
