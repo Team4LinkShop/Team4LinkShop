04163c28b9a0dac17aaaf8a78e2f8924c3fad5b7
April-37 <mm37april@gmail.com>
2024-09-30 15:43:18 +0900

    feat : 리스트 페이지 레이아웃 구분

--- src/pages/ShopList.jsx
+++ src/pages/ShopList.jsx
@@ -10,6 +10,11 @@ function ShopList() {
         <Logo />
         <BtnCreate />
       </div>
+      <div css={containerSearch}></div>
+      <div css={containerDetailFilter}></div>
+      <div css={containerLinkCardList}>
+        <div css={containerLinkCard}></div> {/* 이후 LinkCardList 컴포넌트 내부에 들어갈 부분 */}
+      </div>
     </div>
   );
 }
