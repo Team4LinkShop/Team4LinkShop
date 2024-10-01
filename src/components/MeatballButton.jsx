556f0bdcac393c2893c655ef423233704cefd899
Suzy-Lee <>
2024-10-01 15:58:39 +0900

    feat: 상세페이지 프로필에서 더보기 버튼 클릭 시, 모달창 열고 닫기 기능 추가

--- src/components/MeatballButton.jsx
+++ src/components/MeatballButton.jsx
@@ -1,13 +1,32 @@
 /** @jsxImportSource @emotion/react */
 
+import { useRef, useState } from 'react';
 import icMeatBall from '../assets/images/ic-meatball.svg';
 import { icContainer } from './ShareButton';
 
 function MeatballButton() {
+  const [isOpen, setIsOpen] = useState(false);
+  const menu = useRef(null);
+  const handleCloseMenu = (e) => {
+    if (isOpen && !menu.current?.contains(e.target)) {
+      setIsOpen(false);
+    }
+  };
+
+  document.addEventListener('mousedown', handleCloseMenu);
+
   return (
-    <div css={icContainer}>
-      <img src={icMeatBall} alt="더보기 아이콘" />
-    </div>
+    <>
+      <div ref={menu} css={icContainer} onClick={() => setIsOpen(!isOpen)}>
+        <img src={icMeatBall} alt="더보기 아이콘" />
+      </div>
+      {isOpen && (
+        <ul>
+          <li>수정하기</li>
+          <li>삭제하기</li>
+        </ul>
+      )}
+    </>
   );
 }
 
