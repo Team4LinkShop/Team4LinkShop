a91bfd03b8818796087c1a393ce9712279083712
Suzy-Lee <>
2024-10-02 15:20:58 +0900

    style: 더보기 버튼 클릭 시, Dropdown 메뉴 디자인 추가

--- src/components/MeatballButton.jsx
+++ src/components/MeatballButton.jsx
@@ -3,6 +3,8 @@
 import { useRef, useState } from 'react';
 import icMeatBall from '../assets/images/ic-meatball.svg';
 import { icContainer } from './ShareButton';
+import ProfileDropDown from './ProfileDropDown';
+import { css } from '@emotion/react';
 
 function MeatballButton() {
   const [isOpen, setIsOpen] = useState(false);
@@ -17,15 +19,16 @@ function MeatballButton() {
 
   return (
     <>
-      <div ref={menu} css={icContainer} onClick={() => setIsOpen(!isOpen)}>
+      <div
+        ref={menu}
+        css={css`
+          ${icContainer};
+          position: relative;
+        `}
+        onClick={() => setIsOpen(!isOpen)}>
         <img src={icMeatBall} alt="더보기 아이콘" />
+        {isOpen && <ProfileDropDown />}
       </div>
-      {isOpen && (
-        <ul>
-          <li>수정하기</li>
-          <li>삭제하기</li>
-        </ul>
-      )}
     </>
   );
 }
--- src/components/ProfileDropDown.jsx
+++ src/components/ProfileDropDown.jsx
@@ -1,14 +1,45 @@
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
+
 function ProfileDropDown() {
   return (
-    <ul>
-      <li>
-        <a href="">수정하기</a>
-      </li>
-      <li>
-        <a href="">삭제하기</a>
-      </li>
+    <ul css={dropDownContainer}>
+      <li>수정하기</li>
+      <li>삭제하기</li>
     </ul>
   );
 }
+
+const dropDownContainer = css`
+  background-color: #fff;
+  position: absolute;
+  top: 3.1rem;
+  right: 0;
+  margin: 0;
+  padding: 0%;
+  width: 16rem;
+  text-align: center;
+  box-shadow: 0 0.4rem 1.6rem 0 rgba(17, 34, 17, 0.05);
+  border-radius: 0.6rem;
+
+  > li {
+    padding: 1.8rem 4.6rem;
+    border-top: 1px solid #ddd;
+    border-right: 1px solid #ddd;
+    border-left: 1px solid #ddd;
+    cursor: pointer;
+    font-weight: 500;
+    font-size: 1.8rem;
+    color: #14151a;
+  }
+  > li:first-of-type {
+    border-bottom: none;
+    border-radius: 0.6rem 0.6rem 0 0;
+  }
+  > li:last-of-type {
+    border-bottom: 1px solid #ddd;
+    border-radius: 0 0 0.6rem 0.6rem;
+  }
+`;
 
 export default ProfileDropDown;
