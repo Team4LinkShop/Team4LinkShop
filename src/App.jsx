4a5a38e8873628617b7e21c7c1d0c2a9f9402488
이영찬 <dudcks6813@naver.com>
2024-10-03 14:46:35 +0900

    아이디 검사 및 비밀번호 보기/숨김 활성화

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Name, Id, Password } from './components/common/userinfor.jsx';
+import UserInfo from './components/common/userinfor.jsx';
 import ItemInformation from './components/common/iteminfor.jsx';
 import MyShoppingMall from './components/common/myshoppingmall.jsx';
 import { css } from '@emotion/react';
