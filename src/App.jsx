6738acf8134a87d53561347f7b578a18b3359792
Suzy-Lee <>
2024-10-06 12:12:02 +0900

    refactor: App 파일 내 ProfileDetailPage 페이지 삭제

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
 import { ToastContainer } from 'react-toastify';
-import ProfileDetailPage from './pages/ProfileDetailPage';
 
 function App() {
   return (
     <>
-      <ProfileDetailPage linkShopId={12}/>
       {/* NOTE: 아래 ToastContainer 컴포넌트는 토스트 메세지인데, root에 둬야한대서 둡니다......토스트 메세지 활용하실분 활용하세용! 추후 여유되면 디자인 커스텀 해볼게요!  */}
       <ToastContainer
         position="bottom-right"
